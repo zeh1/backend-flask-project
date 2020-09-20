7f6c70aedd371d7d9afa5ec5f74b8d97f026172f
crispy chili <zeh1xz@gmail.com>
2020-09-19 18:52:20 -0700

    implemented jwt deconstructor service

--- services/jwt_deconstructor_service.py
+++ services/jwt_deconstructor_service.py
@@ -1,25 +1,38 @@
+from encode_decode_service import EncodeDecodeService
+import json
+
+
+
+
+
+TEST_JWT = 'eyJhbGciOiAiaHMyNTYiLCAidHlwIjogImp3dCJ9.eyJwYXlsb2FkIjogMH0=.NjQ3YjAwZTBhNDU3NDM3NGU4NTUxNjYyMTk4ZjEwMjA='
+
+
 
 
 
 class JwtDeconstructorService:
-    def __init__(self, jwt):
+
+
+    def __init__(self, jwt = TEST_JWT):
         self.jwt = jwt
 
+
+
     def get(self):
-        pass
+        return json.loads( self.__parse() )
+
+
 
     def __parse(self):
         array = self.jwt.split('.')
-        def 
-#
+        
+        def transform(jwt_section):
+            d = EncodeDecodeService.encode(jwt_section)
+            d = EncodeDecodeService.b64_to_string(d)
+            return d
+        
+        res = map(transform, array)
 
-
-
-class SignatureCheckerService:
-    def check(jwt = TEST_JWT, secret = SECRET):
-        array = jwt.split('.')
-        hmac_msg = EncodeDecodeService.encode( array[0] + '.' + array[1] )
-        supplied_sig = array[2]
-        proper_sig = EncodeDecodeService.to_b64_string( hmac.new(secret, hmac_msg).hexdigest() )
-        return supplied_sig == proper_sig
+        return list(res)[1]
 #
