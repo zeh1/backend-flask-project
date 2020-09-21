75116f6fee4b59320d34e3021a18fa9db5de3f60
crispy chili <zeh1xz@gmail.com>
2020-09-20 18:35:27 -0700

    implemented password reset attempt

--- services/query_builder_service_v2.py
+++ services/query_builder_service_v2.py
@@ -1,3 +1,5 @@
+import uuid
+
 
 
 
@@ -36,7 +38,7 @@ class QueryBuilderService:
         '''.format(user_id = user_id)
 
         return [insert_post_query, update_post_count_query]
-        # need to handle exceptions: no jwt, invalid jwt
+        # need to handle exceptions: no jwt (None), invalid jwt
 
 
 
@@ -76,4 +78,48 @@ class QueryBuilderService:
 
     # requires a processed (hashed) password
     def signup_attempt(email, username, password):
+        try_to_insert_user = '''
+            insert into users(username, email, password)
+            values({username}, {email}, {password});
+        '''.format(username = username, email = email, password = password)
+        # need to handle exception: UserAlreadyExistsException
+
+        session_id = uuid.uuid4().hex
+        create_verification_session = '''
+            insert into verifications(session_id, user_id)
+            values({session_id}, (select user_id from users where username = {username}));
+        '''.format(session_id = session_id, username = username)
+
+        # retrieve session id in order to send email verification
+        retrieve_session_id = '''
+            select session_id from verifications
+            where user_id = (select user_id from users where username = {username});
+        '''.format(username = username)
+
+        return [try_to_insert_user, create_verification_session, retrieve_session_id]
+
+
+
+    def password_reset_attempt(email):
+        check_if_user_exists = '''
+            select email from users where email = {email};
+        '''.format(email = email)
+        # need to handle exception: user not found
+
+        session_id = uuid.uuid4().hex
+        create_reset_session = '''
+            insert into resets(session_id, user_id)
+            values({session_id}, (select user_id from users where email = {email}));
+        '''.format(session_id = session_id, email = email)
+
+        retrieve_session_id = '''
+            select session_id from resets
+            where user_id = (select user_id from users where email = {email});
+        '''.format(email = email)
+
+        return [check_if_user_exists, create_reset_session, retrieve_session_id]
+
+
+
+    def password_change_attempt(password, new_password):
         
